c87e150c8136741b5c63e344ffe92972ebd5b119
s4nt14g0pl4ys <santsepulveda29@gmail.com>
2024-08-27 18:32:06 -0500

    eliminando

--- main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import mean_var_std as mvs
-
-l_n = np.array([])
-for i in range(9):
-    num = int(input(f"Ingrese el numero {i+1}:"))
-    l_n = np.append(l_n, num)
-result = mvs.calculate(l_n)
-print(result)
